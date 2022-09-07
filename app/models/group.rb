4fac55bce0998574b93176b5169a91e6a7e91a9c
axel soler <99031215+AxelSoler@users.noreply.github.com>
2022-09-07 16:00:52 -0300

    Icons
    Displaying icons through URL links

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,5 +3,5 @@ class Group < ApplicationRecord
   has_and_belongs_to_many :performance
 
   validates :name, presence: true, length: { maximum: 25 }
-  validates :icon, presence: true, length: { maximum: 200 }
+  validates :icon, presence: true, length: { maximum: 200 }, format: URI::DEFAULT_PARSER.make_regexp(%w[http https])
 end
